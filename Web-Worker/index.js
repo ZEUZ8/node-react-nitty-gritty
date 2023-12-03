a9aeb6097bb315f50c8914c59701f653a1d3059b
sinan <ptsinan8590@gmail.com>
2023-12-03 18:51:59 +0530

    Implemented web worker to offload background-changing calculations from the main thread, resolving the issue of blocking when the 'Calculate' button is pressed. This optimization leverages the benefits of multi-threading, freeing the main thread for smoother DOM manipulation.

--- Web-Worker/index.js
+++ Web-Worker/index.js
@@ -1,13 +1,29 @@
+const worker = new Worker('worker.js')
 const sumButton = document.querySelector("#sumBtn")
 const bgButton = document.querySelector("#bgBtn")
 
+
+/*when we click the calculate button, the main thread will block since the calculating a 
+cpu-intensive work so the background changing or any other work will be blocke because of this */
+
+// sumButton.addEventListener("click",(event)=>{
+//     let sum = 0
+//     for(let i=0;i<10000000000;i++){
+//         sum += i
+//     }
+//     alert(`The total Sum is ${sum}`)
+// })
+
+
+//so let's implementh solution the multithread by creating wokers and passing the function to them 
 sumButton.addEventListener("click",(event)=>{
-    let sum = 0
-    for(let i=0;i<10000000000;i++){
-        sum += i
-    }
-    alert(`The total Sum is ${sum}`)
+    worker.postMessage("name")
 })
+
+//getting the result from the worker ans showing it in the alert
+worker.onmessage = function(message){
+    alert(`The total Sum is ${message?.data}`)
+}
 
 bgButton.addEventListener("click",event=>{
     if(document.body.style.background !== "green"){
